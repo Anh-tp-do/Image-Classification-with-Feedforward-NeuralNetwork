66b8182573c4621c51593cd77ccc0213935f5260
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-04-12 10:57:24 -0400

    Create FeedforwardNN.py

---
+++ FeedforwardNN.py
@@ -0,0 +1,45 @@
+class FFnnModel(nn.Module):
+    """Feedfoward neural network with 2 hidden layer"""
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        # hidden layer
+        self.linear1 = nn.Linear(in_size, 1000)
+        self.linear3 = nn.Linear(1000, 100)
+        # output layer
+        self.linear2 = nn.Linear(100, out_size)
+        
+    def forward(self, xb):
+        # Flatten the image tensors
+        xb = xb.view(xb.size(0), -1)
+        # Get intermediate outputs using hidden layer
+        out = self.linear1(xb)
+         # Apply activation function
+        out = F.relu(out)
+        out = self.linear3(out)
+       
+        # Get predictions using output layer
+        out = self.linear2(out)
+        return out
+    
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
