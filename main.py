a82b946d0b8d0e629813d75d112b76bb92858094
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-04-12 11:01:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -76,6 +76,99 @@ for images,labels in train_loader:
     break
 
 
+input_size = 3*256*256
+num_classes = 10
+
+model = FFnnModel(input_size, out_size = num_classes)
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+    
+device = get_default_device()
+device
+
+
+train_loader = DeviceDataLoader(train_loader, device)
+val_loader = DeviceDataLoader(val_loader, device)
+to_device(model, device);
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    torch.cuda.empty_cache()
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        for batch in tqdm(train_loader):
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+//for t in model.parameters():
+//    print(t.shape)
+
+//history = [evaluate(model, val_loader)]
+//history
+
+history += fit(5, 0.05, model, train_loader, val_loader)
+history += fit(5, 0.2, model, train_loader, val_loader)
+history += fit(7, 0.15, model, train_loader, val_loader)
+history += fit(9, 0.05, model, train_loader, val_loader)
+
+
+losses = [x['val_loss'] for x in history]
+plt.plot(losses, '-x')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('Loss vs. No. of epochs');
+
+accuracies = [x['val_acc'] for x in history]
+plt.plot(accuracies, '-x')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.title('Accuracy vs. No. of epochs');
+
+
 
 
 
